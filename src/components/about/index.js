b4201e94850c680a6226b4e468d3a54dfc088515
Julianna Weldon <julweldon@gmail.com>
2018-06-26 19:44:38 -0600

    Update index.js

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -17,10 +17,10 @@ class About extends React.Component {
                 <StyledMain>
                     <p>
                         Week E<sup>0</sup> is the second best week of the year*. Excited for September? Just wait til you see what the EUS has in store for you. 
-                        BBQs, movie nights, pancake breakfasts, and some crazy traditions to be a part of!It’s our grand opening bash, so start off 
+                        BBQs, movie nights, pancake breakfasts, and some crazy traditions to be a part of! It’s our grand opening bash, so start off 
                         your year right by hanging out with your fellow first years for the first week full of back-to-back activities. The second 
-                        week will be a little less hectic, but don’t miss the info session for E-Retreat or the chance to interact with UBC’s various 
-                        engineering student teams and clubs.  We hope to see you around!
+                        week will be a little less busy, especially since classes will be in full swing, but don’t miss the info session for E-Retreat or the chance to interact with UBC’s various 
+                        engineering student teams and clubs. We hope to see you around!
                     </p>
                     <p>
                         <small>* after <a href="https://www.facebook.com/UBCEngineers/videos/762594977229597/">E-Week</a> of course.</small>
