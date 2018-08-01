b12680a5613f9cc952daecbcd3aa37f9a7b20216
Quentin Golsteyn <qgolsteyn@gmail.com>
2018-08-01 09:57:14 -0700

    Improve calendar component

--- src/components/content/Calendar/index.tsx
+++ src/components/content/Calendar/index.tsx
@@ -1,38 +1,52 @@
 import * as React from 'react';
-import { StyledDay, StyledEvent, StyledWrapper } from './styled';
+import { StyledDay, StyledEvent, StyledMuted, StyledSubtitle, StyledTitle, StyledWrapper } from './styled';
 
 interface IProps {
     title: string;
+    days: JSX.Element[];
     events: Array<{
         title: string;
+        subtitle: string;
         day: number;
         start: number;
         end: number;
+        muted?: boolean;
     }>;
 }
 
 export default (props: IProps) => {
     return (
-        <StyledWrapper>
-            <StyledDay style={{fontSize: '1.2em', gridRowStart: 1, gridColumnStart: 1, gridColumnEnd: 5}}>
+        <StyledWrapper style={{gridTemplateColumns: `repeat(${props.days.length}, 1fr)`}}>
+            <StyledDay style={{fontSize: '1.2em', gridRowStart: 1, gridColumnStart: 1, gridColumnEnd: props.days.length + 1}}>
                 {props.title}
             </StyledDay>
-            <StyledDay style={{gridRowStart: 2, gridColumnStart: 1}}>
-                Tuesday Sept 5th
-            </StyledDay>
-            <StyledDay style={{gridRowStart: 2, gridColumnStart: 2}}>
-                Wednesday Sept 6th
-            </StyledDay>
-            <StyledDay style={{gridRowStart: 2, gridColumnStart: 3}}>
-                Thursday Sept 7th
-            </StyledDay>
-            <StyledDay style={{gridRowStart: 2, gridColumnStart: 4}}>
-                Friday Sept 8th
-            </StyledDay>
+            {props.days.map((element, index) => (
+                <StyledDay key={element.toString()} style={{gridRowStart: 2, gridColumnStart: index + 1}}>
+                    {element}
+                </StyledDay>
+            ))}
             {props.events.map((element) => (
-                <StyledEvent key={element.title} style={{gridRowStart: element.start * 2 - 13, gridRowEnd: element.end * 2 - 13, gridColumnStart: element.day - 1}}>
-                    {element.title}
-                </StyledEvent>
+                <>
+                {!element.muted ? 
+                    <StyledEvent key={element.title} style={{gridRowStart: element.start * 2 - 13, gridRowEnd: element.end * 2 - 13, gridColumnStart: element.day}}>
+                        <StyledSubtitle>
+                            {element.subtitle}
+                        </StyledSubtitle>
+                        <StyledTitle>
+                            {element.title}
+                        </StyledTitle>
+                    </StyledEvent>
+                    :
+                    <StyledMuted key={element.title} style={{gridRowStart: element.start * 2 - 13, gridRowEnd: element.end * 2 - 13, gridColumnStart: element.day}}>
+                        <StyledSubtitle>
+                            {element.subtitle}
+                        </StyledSubtitle>
+                        <StyledTitle>
+                            {element.title}
+                        </StyledTitle>
+                    </StyledMuted>
+                }
+                </>
             ))}
         </StyledWrapper>
     )
--- src/components/content/Calendar/styled.ts
+++ src/components/content/Calendar/styled.ts
@@ -2,12 +2,11 @@ import styled from 'styled-components';
 
 export const StyledWrapper = styled.div`
     width: 100%;
-    height: 550px;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: 48px 48px repeat(28, 1fr);
+    grid-template-rows: 48px 48px repeat(28, minmax(8px,min-content));
     grid-gap: 4px;
     font-family: 'Fjalla One', sans-serif;
+    margin-top: 64px;
 `
 
 export const StyledDay = styled.div`
@@ -19,10 +18,11 @@ export const StyledDay = styled.div`
 
 export const StyledEvent = styled.div`
     display: flex;
-    font-size: 0.8em;
+    flex-flow: column nowrap;
     justify-content: center;
     align-items: center;
     background: white;
+    padding: 8px 2px;
     border-radius: 4px;
     box-shadow: rgba(0,0,0,0.4) 0 0 8px;
     color: #E51E25;
@@ -31,5 +31,26 @@ export const StyledEvent = styled.div`
 
     &:hover {
         background: #FAFAFA;
+    }
+`
+
+export const StyledSubtitle = styled.div`
+    font-size: 0.7em;
+    margin-top: 4px;
+`
+
+export const StyledTitle = styled.div`
+    font-size: 1em;
+    margin-top: 4px;
+`
+
+export const StyledMuted = styled(StyledEvent)`
+    background: rgba(255,255,255,0.2);
+    box-shadow: none;
+    color: white;
+    cursor: initial;
+
+    &:hover {
+        background: rgba(255,255,255,0.2);
     }
 `
